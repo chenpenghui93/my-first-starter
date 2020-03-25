823e53a1b8a4c47ed17d655d7d2f0f51dff452b4
chenpenghui <chenpenghui@hisense.com>
2020-03-25 09:29:15 +0800

    initial

---
+++ src/main/java/com/example/myfirststarter/MyFirstStarterApplication.java
@@ -0,0 +1,13 @@
+package com.example.myfirststarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyFirstStarterApplication.class, args);
+    }
+
+}
